e47a0eff50c83a3e8cf18eab9aaa45c4f2a05850
mazenaboelanin <mazenaboelanin@gmail.com>
2022-05-18 19:51:01 +0200

    add ingredients to recpies

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,24 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService{
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('test Recipe', 'thisis desc for a recipe','https://www.simplyrecipes.com/thmb/JWjdE8YwikAae0KZuyy6ZJW7Utw=/3000x2001/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg' ),
-    new Recipe('test Recipe 2', 'this is desc for a recipe 2','https://img.delicious.com.au/5aLcV7cG/del/2021/05/slow-roasted-butter-eggplant-curry-152139-2.jpg' )
+    new Recipe('test Recipe',
+     'thisis desc for a recipe',
+     'https://www.simplyrecipes.com/thmb/JWjdE8YwikAae0KZuyy6ZJW7Utw=/3000x2001/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('test Recipe 2',
+     'this is desc for a recipe 2',
+     'https://img.delicious.com.au/5aLcV7cG/del/2021/05/slow-roasted-butter-eggplant-curry-152139-2.jpg',
+     [
+      new Ingredient('buns', 1),
+      new Ingredient('Meat', 2)
+     ] )
   ];
 
   getRecipes(){
